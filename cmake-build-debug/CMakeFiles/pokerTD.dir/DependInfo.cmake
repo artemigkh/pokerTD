4f6caa500f4dfc0e350c6f7f3982a89a98f927cf
Artem Kholodov <artemigkh@gmail.com>
2017-10-01 15:06:41 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/pokerTD.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Artem/pokerTD/src/main.cpp" "C:/Users/Artem/pokerTD/cmake-build-debug/CMakeFiles/pokerTD.dir/src/main.cpp.obj"
+  "C:/Users/Artem/pokerTD/src/testfolder/Car.cpp" "C:/Users/Artem/pokerTD/cmake-build-debug/CMakeFiles/pokerTD.dir/src/testfolder/Car.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../SFML-2.4.2/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/pokerTD.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pokerTD.dir/src/main.cpp.obj"
+  "CMakeFiles/pokerTD.dir/src/testfolder/Car.cpp.obj"
+  "pokerTD.pdb"
+  "pokerTD.exe"
+  "pokerTD.exe.manifest"
+  "libpokerTD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pokerTD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
